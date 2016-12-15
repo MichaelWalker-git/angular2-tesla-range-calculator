995dcf4cc660f9dc0a775dd1ee00286e99f7c920
Michael <michaelhuytran@gmail.com>
2016-12-15 10:33:17 -0800

    [feature] - adds components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TeslaBatteryComponent } from './tesla-battery/tesla-battery.component';
+import { BatteryService } from './tesla-battery/tesla-battery.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeslaBatteryComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    BatteryService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/tesla-battery/tesla-battery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tesla-battery',
+  templateUrl: './tesla-battery.component.html',
+  styleUrls: ['./tesla-battery.component.css']
+})
+export class TeslaBatteryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
