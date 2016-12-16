c52a49c611dd0c4d1eee1fc687ed9efbd5d427d5
Michael <michaelhuytran@gmail.com>
2016-12-15 22:11:43 -0800

    [feature] - heating/air

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BatteryService } from './containers/tesla-battery/tesla-battery.service
 import { TeslaCarComponent } from './components/tesla-car/tesla-car.component';
 import { TeslaStatsComponent } from './components/tesla-stats/tesla-stats.component';
 import { TeslaCounterComponent } from './components/tesla-counter/tesla-counter.component';
+import { TeslaClimateComponent } from './components/tesla-climate/tesla-climate.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { TeslaCounterComponent } from './components/tesla-counter/tesla-counter.
     TeslaBatteryComponent,
     TeslaCarComponent,
     TeslaStatsComponent,
-    TeslaCounterComponent
+    TeslaCounterComponent,
+    TeslaClimateComponent
   ],
   imports: [
     BrowserModule,
